dc20dc4a21977276fb79282fd19dbeb36e338e6f
Ryland <ryland.scarth233@gmail.com>
2021-04-09 17:16:30 -0500

    Function Parser updated to handle more edge cases and potential errors

--- VectorField/src/FunctionParser.cpp
+++ VectorField/src/FunctionParser.cpp
@@ -3,13 +3,16 @@
 
 namespace parser {
 
-	void Parse(const std::string& s, const int& d, std::unordered_map<std::string, int>& vars, std::string* funcs, int& err) {
+	void Parse(const std::string& s, const int& d, std::unordered_map<std::string, int>& vars, std::string* funcs) {
 		unsigned int s_len = s.length();
 		std::string temp;
 		int c = 0;
+		bool var_dup = false, func_dup = false;
 
 		for (unsigned int i = 0; i <= s_len; i++) {
-			if (s[i] == '(') {
+			if (s[i] == '(' && var_dup != true) {
+				var_dup = true;
+				c = 0;
 				i++;
 				while (s[i - 1] != ')' && i != s_len) {
 					if (s[i] != ',' && s[i] != ')') {
@@ -17,33 +20,37 @@ namespace parser {
 							temp += s[i];
 						}
 					}
-					else {
-						if (c < d-1) {
-							//std::cout << "Inserting (" << temp << ", " << c << ") into map<> vars" << std::endl;
-							vars.insert(std::pair<std::string, int>(temp, c));
-						}
-						else {
-							error::printError("More variables than dimensions parsed, check delimiters"), i;
-							err = -210;
-							break;
-						}
-
-						c++;
-						temp = "";
+					else if (c >= (d - 1) && s[i] == ',') {
+						std::cout << s << std::endl;
+						error::printError("More variables than dimensions parsed, check delimiters", -300, i);
+						break;
 					}
+					else {
+						vars.insert(std::pair<std::string, int>(temp, c));
+						temp = "";
+						c++;
+					}
+
 					i++;
 				}
 
-				if (s[i - 1] != ')') {
-					error::printError("No closing bracket found for variables", i);
-					err = -211;
+				if (s[i-1] != ')' && s[i] != ',') {
+					std::cout << s << std::endl;
+					error::printError("No closing bracket found for variables", -310, i);
 					break;
 				}
 
 				temp = "";
-
 			}
-			else if (s[i] == '<' && c == d) {
+			else if (s[i] == '(' && var_dup == true) {
+				std::cout << s << std::endl;
+				error::printError("Duplicate '( )' brackets parsed", -340, i);
+				break;
+			}
+
+			if (s[i] == '<' && func_dup != true) {
+				func_dup = true;
+				c = 0;
 				i++;
 				while (s[i - 1] != '>' && i != s_len) {
 					if (s[i] != ',' && s[i] != '>') {
@@ -51,48 +58,52 @@ namespace parser {
 							temp += s[i];
 						}
 					}
-					else {
-						if (c < (d * 2)) {
-							//std::cout << "Appending " << temp << " to funcs[" << c - d << "]" << std::endl;
-							funcs[c - d] = temp;
-						}
-						else {
-							error::printError("More functions than dimensions parsed, check delimeters", i);
-							err = -220;
-							break;
-						}
-
-						c++;
-						temp = "";
+					else if (c >= (d - 1) && s[i] == ',') {
+						std::cout << s << std::endl;
+						error::printError("More functions than dimensions parsed, check delimeters", -320, i);
+						break;
 					}
+					else {
+						funcs[c] = temp;
+						temp = "";
+						c++;
+					}
+
 					i++;
 				}
 
 				if (s[i - 1] != '>') {
-					error::printError("No closing bracket found for functions", i);
-					err = -221;
+					std::cout << s << std::endl;
+					error::printError("No closing bracket found for functions", -330, i);
 					break;
 				}
+
+				temp = "";
 			}
-			else if (s[i] == '<' && c != d) {
-				error::printError("Functions and variables out of order", i);
-				err = -230;
+			else if (s[i] == '<' && func_dup == true) {
+				std::cout << s << std::endl;
+				error::printError("Duplicate '< >' brackets parsed", -350, i);
 				break;
 			}
 		}
 	}
 
-	void Interpreter(const int& d, std::unordered_map<std::string, int>& vars, std::string* funcs[], int& err) {
+	void Interpreter(const int& d, std::unordered_map<std::string, int>& vars, std::string* funcs[]) {
 
 	}
 
 	//Takes in Function from config.json and dimension count <2,3>
-	void ParserHandler(std::string& s, const int& d, int& err) {
+	void ParserHandler(std::string& s, const int& d) {
 		std::unordered_map<std::string, int> vars;
 		std::string* funcs = new std::string[d];
 
-		Parse(s, d, vars, funcs, err);
-		if (err < 0) return;
+		std::cout << "\nDimensions = " << d << std::endl;
+		Parse(s, d, vars, funcs);
+		if (error::err < 0) return;
+
+		for (auto& x : vars) {
+			std::cout << x.first << ": " << x.second << std::endl;
+		}
 
 		for (int i = 0; i < d; i++) {
 			std::cout << funcs[i] << std::endl;
--- VectorField/src/FunctionParser.hpp
+++ VectorField/src/FunctionParser.hpp
@@ -3,7 +3,7 @@
 #include "pch.h"
 
 namespace parser {
-	void Parse(const std::string& s, const int& d, std::unordered_map<std::string, int>& vars, std::string* funcs, int& err);
-	void Interpreter(const int& d, std::unordered_map<std::string, int>& vars, std::string* funcs[], int& err);
-	void ParserHandler(std::string& s, const int& d, int& err);
+	void Parse(const std::string& s, const int& d, std::unordered_map<std::string, int>& vars, std::string* funcs);
+	void Interpreter(const int& d, std::unordered_map<std::string, int>& vars, std::string* funcs[]);
+	void ParserHandler(std::string& s, const int& d);
 }
